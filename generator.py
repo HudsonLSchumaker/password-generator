ed5a83cc645a44cbaca67559324d2be6cef3aac9
Hudson Schumaker <93709248+HudsonLSchumaker@users.noreply.github.com>
2022-08-26 18:39:54 +0200

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,5 +1,5 @@
 
-import random
+import random 
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
            'n', 'o', 'p', 'q', 'w', 'r', 's', 't', 'u', 'v', 'y', 'x', 'z']
